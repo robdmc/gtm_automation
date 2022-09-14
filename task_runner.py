c5ac98073da413ffe30ccbd3bc136a3e667b1aaf
Generic User <generic@user.net>
2022-09-14 14:53:26 -0400

    wip

--- task_runner.py
+++ task_runner.py
@@ -1,41 +1,16 @@
 #! /usr/bin/env python
 
-# import os
-# import datetime
 import click
 import importlib
-# import gtmarket as gtm
-# from rocketry import Rocketry
-# import easier as ezr
+from rocketry import Rocketry
+import easier as ezr
 
 # Will want to write to logs for daemon process
-# logger = ezr.get_logger('snapper')
-# app = Rocketry()
+logger = ezr.get_logger('task_runner')
+app = Rocketry()
 
 
 
-# def snap():
-#     # Set this to True when debugging to minimize salesforce hits
-#     use_cache = False
-
-#     # These are the names of the objects we want to snapshot
-#     object_names = ['opportunity', 'account', 'order']
-
-#     # This is the day we are snapshotting on
-#     today = str(datetime.datetime.now().date())
-
-#     # Loop over all names
-#     for object_name in object_names:
-#         # When running in daemon mode, you don't want to barf on error, just
-#         # write errors to log
-#         try:
-#             # Run the snapper and tell the log about it
-#             snapper = ObjectSnapper(object_name, use_cache=use_cache)
-#             snapper.run()
-#             logger.info(f'date: {today}  sucess for: {object_name}')
-#         except:  # noqa
-#             # If an error happened, tell the log about it
-#             logger.exception(f'date: {today} error for: {object_name}\n')
 
 
 # @app.task('every 6 hours')
@@ -46,10 +21,11 @@ import importlib
 #     snap()
 
 
+@app.task('every 3 hours')
 def run():
     module_list = [
-        # 'tasks.snapshot_sfdc',
-        # 'tasks.snapshot_predictor',
+        'tasks.snapshot_sfdc',
+        'tasks.snapshot_predictor',
         'tasks.snapshot_dash_data',
     ]
     for module_name in module_list:
@@ -67,13 +43,12 @@ def main(daemon):
     """
     Handle the cli arguments and run in the appropriate mode
     """
-    run()
-    # if daemon:
-    #     logger.info('Starting daemon mode')
-    #     app.run()
-    # else:
-    #     logger.info('Running single snapshot')
-    #     snap()
+    if daemon:
+        logger.info('Starting daemon mode')
+        app.run()
+    else:
+        logger.info('Running single snapshot')
+        run()
 
 
 if __name__ == '__main__':
