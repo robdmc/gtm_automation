67782b06b84f38b5e362c4ce3b2cab6c6dd1526c
Generic User <generic@user.net>
2022-09-23 14:34:12 -0400

    wip

--- dashboard/tasks/snapshot_predictor.py
+++ dashboard/tasks/snapshot_predictor.py
@@ -1,5 +1,6 @@
 import easier as ezr
 import gtmarket as gtm
+import os
 
 # Will want to write to logs for daemon process
 logger = ezr.get_logger('snap_forecast')
@@ -10,4 +11,8 @@ def run():
     mp.fit()
     mp_hist = gtm.ModelParamsHist()
     mp_hist.store(mp)
-    logger.info('success for fitting model params')
+    logger.info('success for fitting/updating model params')
+
+    translator = gtm.GTMTranslator(local_file=os.path.expanduser('~/data/gtm_databasae.sqlite'))
+    translator.postgres_to_sqlite()
+    logger.info('success backing up gtm database')
